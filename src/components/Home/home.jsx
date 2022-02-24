17aa74e85cc6c3f8f62543fc1d82648a431cea50
Aditya kumar <adityakamal303@gmail.com>
2022-02-24 14:42:06 +0530

    check

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -13,7 +13,7 @@ import {Link} from 'react-router-dom'
 const Home = () => {
   return (
     <>
-    
+    {/* asdfg */}
     <section className='landing-page'>
       <header>
       <div className="col-12 col-s-12 video-container">
